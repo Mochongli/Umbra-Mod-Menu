15cee57961f2e31128b8fdba9d609afb8adbe5bd
Mochongli <36944889+Mochongli@users.noreply.github.com>
2020-08-19 19:57:31 +0800

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,4 +1,4 @@
-﻿// TODO LIST
+// TODO LIST
 /*
 Add filters to ESPs?
 Make ESP less laggy??
@@ -14,13 +14,13 @@ using RoR2;
 using UnityEngine.SceneManagement;
 using System.Linq;
 
-namespace UmbraRoR
+namespace Poke
 {
     public class Main : MonoBehaviour
     {
         public const string
-            NAME = "U M B R A",
-            VERSION = "1.3.0";
+            NAME = "K K P",
+            VERSION = "";
 
         public static string log = "[" + NAME + "] ";
 
@@ -131,15 +131,15 @@ namespace UmbraRoR
         {
             if (Updates.updateAvailable)
             {
-                GUI.Label(new Rect(Screen.width - 100, 1f, 100, 50f), $"Umbra Menu (v{VERSION}) <color=grey>-</color> <color=yellow>Lastest (v{Updates.latestVersion})</color>", WatermarkStyle);
+                GUI.Label(new Rect(Screen.width - 100, 1f, 100, 50f), $"Poke  (v{VERSION}) <color=grey>-</color> <color=yellow>Lastest (v{Updates.latestVersion})</color>", WatermarkStyle);
             }
             else if (Updates.upToDate)
             {
-                GUI.Label(new Rect(Screen.width - 100, 1f, 100, 50f), $"Umbra Menu (v{VERSION})", WatermarkStyle);
+                GUI.Label(new Rect(Screen.width - 100, 1f, 100, 50f), $"Poke  (v{VERSION})", WatermarkStyle);
             }
             else if (Updates.devBuild)
             {
-                GUI.Label(new Rect(Screen.width - 100, 1f, 100, 50f), $"Umbra Menu (v{VERSION}) <color=grey>-</color> <color=yellow>Dev Build</color>", WatermarkStyle);
+                GUI.Label(new Rect(Screen.width - 100, 1f, 100, 50f), $"Poke  (v{VERSION}) <color=grey>-</color> <color=yellow>Dev Build</color>", WatermarkStyle);
             }
 
             #region GenerateMenus
@@ -1169,15 +1169,15 @@ namespace UmbraRoR
 
             if (Updates.updateAvailable)
             {
-                GUI.Label(new Rect(mainRect.x + 5f, mainRect.y + 5f, widthSize + 5, 85f), $"U M B R A \n<color=yellow>O U T D A T E D</color>", TitleStyle);
+                GUI.Label(new Rect(mainRect.x + 5f, mainRect.y + 5f, widthSize + 5, 85f), $"Poke \n<color=yellow>O U T D A T E D</color>", TitleStyle);
             }
             else if (Updates.upToDate)
             {
-                GUI.Label(new Rect(mainRect.x + 5f, mainRect.y + 5f, widthSize + 5, 85f), $"U M B R A \n<color=grey>v{VERSION}</color>", TitleStyle);
+                GUI.Label(new Rect(mainRect.x + 5f, mainRect.y + 5f, widthSize + 5, 85f), $"Poke \n<color=grey>v{VERSION}</color>", TitleStyle);
             }
             else if (Updates.devBuild)
             {
-                GUI.Label(new Rect(mainRect.x + 5f, mainRect.y + 5f, widthSize + 5, 85f), $"U M B R A \n<color=yellow>D E V</color>", TitleStyle);
+                GUI.Label(new Rect(mainRect.x + 5f, mainRect.y + 5f, widthSize + 5, 85f), $"Poke \n<color=yellow>D E V</color>", TitleStyle);
             }
 
             if (!_CharacterCollected)
